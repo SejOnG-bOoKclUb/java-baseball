906fab51d79bcf574a7690f42bce400c2dff5fc7
hoyeon <highoption@naver.com>
2023-12-12 20:07:47 +0900

    feat(InputView) : inputRestart 메서드 생성

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,11 +1,15 @@
 package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
-    public static String inputNumber(){
+    public static String inputNumber() {
         System.out.println("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
+    public static String inputRestart() {
+        return Console.readLine();
+    }
 }
