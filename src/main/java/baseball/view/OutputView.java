ad4ce33f50a15977caa49fa6279d46649ce557de
hoyeon <highoption@naver.com>
2023-12-12 19:45:26 +0900

    feat(OutputView) : print 메서드들 선언
    - 볼, 스트라이크, 낫싱을 출력하는 메서드 선언

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,7 +2,15 @@ package baseball.view;
 
 public class OutputView {
 
-    public static void startGame(){
+    public static void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printStrikeAndBall(int strike, int ball) {
+        System.out.println(ball + "볼 " + strike + "스트라이크");
+    }
+
+    public static void printNothing() {
+        System.out.println("낫싱");
     }
 }
