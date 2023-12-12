16e14659e5d6c802817a301bd990ae98a1bd5f04
hoyeon <highoption@naver.com>
2023-12-12 19:39:29 +0900

    feat(GameController) : startGame 메서드 선언
    - 게임 시작 기능 생성

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,14 +1,22 @@
 package baseball.controller;
 
+import baseball.model.ComputerNumber;
 import baseball.model.UserNumber;
+import java.util.List;
 
 public class GameController {
 
     private final UserNumber userNumber;
+    private List<Integer> computerNumber;
 
     public GameController() {
         this.userNumber = InputController.makeUserNumbers();
+        this.computerNumber = new ComputerNumber().makeComputerNumber();
     }
 
+    public void startGame() {
+        int countStrike = userNumber.countStrike(computerNumber);
+        int countBall = userNumber.countBall(computerNumber);
+    }
 
 }
