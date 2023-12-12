c4fc0b5d997c91d93cf077c578fe76d276932f6e
hoyeon <highoption@naver.com>
2023-12-12 18:57:31 +0900

    feat(InputController) : makeUserNumber 메서드 정의
    - 인풋 값을 입력받아 검증 이후 올바른 UserNumber를 리턴

--- src/main/java/baseball/controller/InputController.java
+++ src/main/java/baseball/controller/InputController.java
@@ -1,7 +1,16 @@
 package baseball.controller;
 
+import baseball.model.UserNumber;
+import baseball.view.InputView;
+import java.util.List;
+
 public class InputController {
 
+    public UserNumber makeUserNumbers(){
+        String inputNumber = InputView.inputNumber();
+        UserNumber userNumber = new UserNumber(inputNumber);
 
+        return userNumber;
+    }
 
 }
