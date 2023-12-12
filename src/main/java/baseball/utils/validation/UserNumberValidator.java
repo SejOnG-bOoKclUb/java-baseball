28d1998480526a1a64f56902ce59b78e9a05d3fb
hoyeon <highoption@naver.com>
2023-12-12 19:11:50 +0900

    feat(UserNumberValidator) : isCorrectSize 메서드 선언
    - 인풋 값이 세자리 수인지 확인

--- src/main/java/baseball/utils/validation/UserNumberValidator.java
+++ src/main/java/baseball/utils/validation/UserNumberValidator.java
@@ -1,5 +1,8 @@
 package baseball.utils.validation;
 
+import baseball.utils.Convert;
+import java.util.List;
+
 public class UserNumberValidator {
     public void validate(String inputNumber) {
         isInteger(inputNumber);
@@ -7,6 +10,13 @@ public class UserNumberValidator {
 
     private void isInteger(String inputNumber) {
         if (!inputNumber.matches("^[1-9]*$")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isCorrectSize(String inputNumber) {
+        List<Integer> userNumbers = Convert.convertStringToList(inputNumber);
+        if (userNumbers.size() != 3) {
             throw new IllegalArgumentException();
         }
     }
