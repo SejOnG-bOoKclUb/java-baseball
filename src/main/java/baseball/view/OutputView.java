ef940ac1dfeafe2e9383b4d30272dc6872ffdfa1
hoyeon <highoption@naver.com>
2023-12-12 20:07:17 +0900

    add(OutputView) : OutputView 메서드 수정
    - 전체 갯수를 세는 로직 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,10 +7,22 @@ public class OutputView {
     }
 
     public static void printStrikeAndBall(int strike, int ball) {
-        System.out.println(ball + "볼 " + strike + "스트라이크");
-    }
-
-    public static void printNothing() {
-        System.out.println("낫싱");
+        StringBuilder stringBuilder = new StringBuilder();
+        if (ball > 0) {
+            stringBuilder.append(ball);
+            stringBuilder.append("볼 ");
+        }
+        if (strike > 0) {
+            stringBuilder.append(strike);
+            stringBuilder.append("스트라이크");
+        }
+        if (ball == 0 && strike == 0) {
+            stringBuilder.append("낫싱");
+        }
+        if (strike == 3) {
+            stringBuilder.append("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+                    + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        }
+        System.out.println(stringBuilder);
     }
 }
